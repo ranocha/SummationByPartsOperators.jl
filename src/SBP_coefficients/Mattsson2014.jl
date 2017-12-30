97636740429216d87c668620b499836ef4efecca
Hendrik Ranocha <h.ranocha@tu-bs.de>
2017-12-30 09:20:42 +0100

    WIP: Mattsson2014

--- src/SBP_coefficients/Mattsson2014.jl
+++ src/SBP_coefficients/Mattsson2014.jl
@@ -22,11 +22,11 @@ end
 function first_derivative_coefficients(::Mattsson2014, order::Int, T=Float64, parallel=Val{:serial}())
     if order == 2
         left_boundary = (
-            DerivativeCoefficientRow{T,1,2}(SVector(-one(T), one(T))),
+            # d1
+            DerivativeCoefficientRow{T,1,3}(SVector(T(-1),
+                                                    T(1) )),
         )
-        right_boundary = (
-            -left_boundary[1],
-        )
+        right_boundary = .- left_boundary
         upper_coef = SVector(T(1//2))
         central_coef = zero(T)
         lower_coef = -upper_coef
@@ -60,6 +60,7 @@ function second_derivative_coefficients(::Mattsson2014, order::Int, T=Float64, p
         left_weights = SVector(T(1//2))
         right_weights = left_weights
         left_boundary_derivatives = (
+            # first derivative
             DerivativeCoefficientRow{T,1,3}(SVector(T(-3//2),
                                                     T(2),
                                                     T(-1//2) )),
@@ -80,9 +81,62 @@ function third_derivative_coefficients(::Mattsson2014, order::Int, T=Float64, pa
     if order == 2
         left_boundary = (
             # d1
+            DerivativeCoefficientRow{T,1,4}(SVector(T(1//4),
+                                                    T(-5//8),
+                                                    T(1//2),
+                                                    T(-1//8) )),
+            # d2
+            DerivativeCoefficientRow{T,1,4}(SVector(T(-11//16),
+                                                    T(2),
+                                                    T(-31//16),
+                                                    T(5//8) )),
+            # d3
+            DerivativeCoefficientRow{T,1,5}(SVector(T(-1//2),
+                                                    T(15//16),
+                                                    T(1//8),
+                                                    T(-17//16),
+                                                    T(1//2) )),
+        )
+        right_boundary = .- left_boundary
+        upper_coef = SVector(T(-1), T(1//2))
+        central_coef = T(0)
+        lower_coef = -upper_coef
+        left_weights = SVector(T(1//2))
+        right_weights = left_weights
+        left_boundary_derivatives = (
+            # first derivative
+            DerivativeCoefficientRow{T,1,3}(SVector(T(-3//2),
+                                                    T(2),
+                                                    T(-1//2) )),
+            # second derivative
             DerivativeCoefficientRow{T,1,3}(SVector(T(1),
                                                     T(-2),
                                                     T(1) )),
+        )
+        right_boundary_derivatives = (
+            -left_boundary_derivatives[1],
+            left_boundary_derivatives[2]
+        )
+
+        DerivativeCoefficients(left_boundary, right_boundary,
+                                left_boundary_derivatives, right_boundary_derivatives,
+                                lower_coef, central_coef, upper_coef,
+                                left_weights, right_weights, parallel, 3, order, source)
+    else
+        throw(ArgumentError("Order $order not implemented/derived."))
+    end
+end
+
+
+function fourth_derivative_coefficients(::Mattsson2014, order::Int, T=Float64, parallel=Val{:serial}())
+    if order == 2
+        left_boundary = (
+            # d1
+            DerivativeCoefficientRow{T,1,4}(SVector(T(),
+                                                    T(),
+                                                    T(),
+                                                    T() )),
+            # d2
         )
         right_boundary = left_boundary
         upper_coef = SVector(T(1))
@@ -91,6 +145,7 @@ function third_derivative_coefficients(::Mattsson2014, order::Int, T=Float64, pa
         left_weights = SVector(T(1//2))
         right_weights = left_weights
         left_boundary_derivatives = (
+            # first derivative
             DerivativeCoefficientRow{T,1,3}(SVector(T(-3//2),
                                                     T(2),
                                                     T(-1//2) )),
