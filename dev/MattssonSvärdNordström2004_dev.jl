6dfc771e12321c8507567e22999ef9dff7b2b786
Hendrik Ranocha <h.ranocha@tu-bs.de>
2018-01-05 09:55:35 +0100

    MattssonSvärdNordström2004: Dissipation of order 4

--- dev/MattssonSvärdNordström2004_dev.jl
+++ dev/MattssonSvärdNordström2004_dev.jl
@@ -1,6 +1,6 @@
 using SymPy
 
-N = 15
+N = 11
 
 b = [symbols("b_$i", real=true) for i in 1:N]
 B = diagm(b)
@@ -22,3 +22,26 @@ display(B1)
 
 display(D1' * B * D1)
 display(D1' * B1 * D1)
+
+
+# 4th order dissipation
+D2 = zeros(Int, N, N)
+for i in 1:N
+    D2[i,i] = -2
+end
+for i in 1:N-1
+    D2[i+1,i] = 1
+    D2[i,i+1] = 1
+end
+boundary_width = 1
+for i in 1:boundary_width
+    D2[i,:] = D2[boundary_width+1,:]
+    D2[end-i+1,:] = D2[end-boundary_width,:]
+end
+display(D2)
+
+B2 = eye(Int, N); B2[1,1] = B2[end,end] = 0
+display(B2)
+
+display(D2' * B * D2)
+display(D2' * B2 * D2)
--- src/SBP_coefficients/MattssonSvärdNordström2004.jl
+++ src/SBP_coefficients/MattssonSvärdNordström2004.jl
@@ -20,7 +20,6 @@ end
 function dissipation_coefficients(source::MattssonSvärdNordström2004, order::Int, grid, left_weights, right_weights, parallel=Val{:serial}())
     T = eltype(grid)
     if order == 2
-        #=
         boundary_length = 1
         if length(left_weights) < boundary_length
             left_weights = (left_weights..., ntuple(j->one(T), boundary_length-length(left_weights)))
@@ -28,7 +27,6 @@ function dissipation_coefficients(source::MattssonSvärdNordström2004, order::I
         if length(right_weights) < boundary_length
             right_weights = (right_weights..., ntuple(j->one(T), boundary_length-length(right_weights)))
         end
-        =#
 
         left_boundary = (
             # d1
@@ -37,7 +35,7 @@ function dissipation_coefficients(source::MattssonSvärdNordström2004, order::I
                 DerivativeCoefficientRow{T,-1,2}(SVector(T(1), T(-1) )),
             ),
         )
-        for i in 2:length(left_weights)
+        for i in boundary_length+1:length(left_weights)
             di = left_weights[i] .\ (
                 ntuple(j->DerivativeCoefficientRow{T,0,1}(SVector(T(0), )), i-2)...,
                 DerivativeCoefficientRow{T,1,1}(SVector(T(-1), )),
@@ -54,7 +52,7 @@ function dissipation_coefficients(source::MattssonSvärdNordström2004, order::I
                 DerivativeCoefficientRow{T,1,1}(SVector(T(-1), )),
             ),
         )
-        for i in 2:length(right_weights)
+        for i in boundary_length+1:length(right_weights)
             di = right_weights[i] .\ (
                 ntuple(j->DerivativeCoefficientRow{T,0,1}(SVector(T(0), )), i-2)...,
                 DerivativeCoefficientRow{T,0,1}(SVector(T(-1), )),
@@ -77,6 +75,100 @@ function dissipation_coefficients(source::MattssonSvärdNordström2004, order::I
                                        parallel, order, 2, source)
         b = ones(grid)
         b[1] = T(0)
+    elseif order == 4
+        boundary_length = 3
+        if length(left_weights) < boundary_length
+            left_weights = (left_weights..., ntuple(j->one(T), boundary_length-length(left_weights)))
+        end
+        if length(right_weights) < boundary_length
+            right_weights = (right_weights..., ntuple(j->one(T), boundary_length-length(right_weights)))
+        end
+
+        left_boundary = (
+            # d1
+            left_weights[1] .\ (
+                DerivativeCoefficientRow{T,0,2}(SVector(T(1), T(1) )),
+                DerivativeCoefficientRow{T,-1,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-2,2}(SVector(T(1), T(1) )),
+            ),
+            # d2
+            left_weights[2] .\ (
+                DerivativeCoefficientRow{T,0,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-1,3}(SVector(T(4), T(4), T(1) )),
+                DerivativeCoefficientRow{T,-2,3}(SVector(T(-2), T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-1,1}(SVector(T(1), )),
+            ),
+            # d3
+            left_weights[3] .\ (
+                DerivativeCoefficientRow{T,0,2}(SVector(T(1), T(1) )),
+                DerivativeCoefficientRow{T,-1,3}(SVector(T(-2), T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-2,4}(SVector(T(1), T(1), T(4), T(1) )),
+                DerivativeCoefficientRow{T,-1,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-1,1}(SVector(T(1), )),
+            ),
+        )
+        for i in boundary_length+1:length(left_weights)
+            di = left_weights[i] .\ (
+                ntuple(j->DerivativeCoefficientRow{T,0,1}(SVector(T(0), )), i-boundary_length)...,
+                DerivativeCoefficientRow{T,1,1}(SVector(T(1), )),
+                DerivativeCoefficientRow{T,0,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-1,3}(SVector(T(1), T(4), T(1) )),
+                DerivativeCoefficientRow{T,-1,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,0,1}(SVector(T(1), )),
+            )
+            left_boundary = (left_boundary..., di)
+        end
+
+        right_boundary = (
+            # d1
+            right_weights[1] .\ (
+                DerivativeCoefficientRow{T,-1,2}(SVector(T(1), T(1) )),
+                DerivativeCoefficientRow{T,0,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,1,2}(SVector(T(1), T(1) )),
+            ),
+            # d2
+            right_weights[2] .\ (
+                DerivativeCoefficientRow{T,-1,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-1,3}(SVector(T(1), T(4), T(4) )),
+                DerivativeCoefficientRow{T,0,3}(SVector(T(-2), T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-1,1}(SVector(T(1), )),
+            ),
+            # d3
+            right_weights[3] .\ (
+                DerivativeCoefficientRow{T,-1,2}(SVector(T(1), T(1) )),
+                DerivativeCoefficientRow{T,-1,3}(SVector(T(-2), T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-1,4}(SVector(T(1), T(4), T(1), T(1) )),
+                DerivativeCoefficientRow{T,0,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,1,1}(SVector(T(1), )),
+            ),
+        )
+        for i in boundary_length+1:length(right_weights)
+            di = right_weights[i] .\ (
+                ntuple(j->DerivativeCoefficientRow{T,0,1}(SVector(T(0), )), i-boundary_length)...,
+                DerivativeCoefficientRow{T,-1,1}(SVector(T(1), )),
+                DerivativeCoefficientRow{T,-1,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,-1,3}(SVector(T(1), T(4), T(1) )),
+                DerivativeCoefficientRow{T,0,2}(SVector(T(-2), T(-2) )),
+                DerivativeCoefficientRow{T,1,1}(SVector(T(1), )),
+            )
+            right_boundary = (right_boundary..., di)
+        end
+
+        lower_coef = (
+            DerivativeCoefficientRow{T,0,2}(SVector(T(-2), T(-2) )),
+            DerivativeCoefficientRow{T,1,1}(SVector(T(1), )),
+        )
+        central_coef = DerivativeCoefficientRow{T,-1,3}(SVector(T(1), T(4), T(1) ))
+        upper_coef = (
+            DerivativeCoefficientRow{T,-1,2}(SVector(T(-2), T(-2) )),
+            DerivativeCoefficientRow{T,0,1}(SVector(T(1), )),
+        )
+
+        coef = DissipationCoefficients(left_boundary, right_boundary,
+                                       lower_coef, central_coef, upper_coef,
+                                       parallel, order, 2, source)
+        b = ones(grid)
+        b[1] = b[end] = T(0)
     else
         throw(ArgumentError("Order $order not implemented/derived."))
     end
