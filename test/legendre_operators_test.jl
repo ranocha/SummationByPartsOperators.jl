5a6e4ec3d57e89cf496094856e5e2a6815312671
Hendrik Ranocha <h.ranocha@tu-bs.de>
2018-02-02 09:38:13 +0100

    legendre_operators_test.jl

---
+++ test/legendre_operators_test.jl
@@ -0,0 +1,32 @@
+using Base.Test, SummationByPartsOperators
+
+
+function accuracy_test!(res, ufunc, dufunc, D)
+    u = compute_coefficients(ufunc, D)
+    du = compute_coefficients(dufunc, D)
+    A_mul_B!(res, D, u)
+    maximum(abs, du-res) < 5*length(res)*eps(eltype(res))
+end
+
+# Accuracy tests.
+for T in (Float32, Float64)
+    xmin = -one(T)
+    xmax = one(T)
+
+    for N in 2 .^ (1:4)
+        D = legendre_derivative_operator(xmin, xmax, N)
+        println(DevNull, D)
+        @test SummationByPartsOperators.derivative_order(D) == 1
+        @test issymmetric(D) == false
+        u = compute_coefficients(zero, D)
+        res = D*u
+        for k in 1:N-1
+            ufunc = x -> x^k/typeof(x)(gamma(k+1))
+            dufunc = x -> x^(k-1)/typeof(x)(gamma(k))
+            @test accuracy_test!(res, ufunc, dufunc, D)
+            xplot, duplot = evaluate_coefficients(res, D)
+            @test maximum(abs, duplot - dufunc.(xplot)) < 5N*eps(T)
+            @test abs(integrate(u, D)) < N*eps(T)
+        end
+    end
+end
