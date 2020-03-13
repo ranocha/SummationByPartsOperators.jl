605f9e4c3792854782fbb2d254b4f97e0d002d54
Hendrik Ranocha <mail@ranocha.de>
2020-03-13 10:27:45 +0300

    add tests for quartic nonconvex flux

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,7 @@ using Test
     include("conservation_laws/burgers_test.jl")
     include("conservation_laws/cubic_test.jl")
     include("conservation_laws/variable_linear_advection_test.jl")
+    include("conservation_laws/quartic_nonconvex_test.jl")
 end
 @time @testset "Second Order Equations" begin
     include("second_order_eqs/wave_eq_test.jl")
