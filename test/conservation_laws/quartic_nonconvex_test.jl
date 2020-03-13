76614265cc499f3a3e966a7e1bb2dfc690415cb3
Hendrik Ranocha <mail@ranocha.de>
2020-03-13 09:53:15 +0300

    test for quartic nonconvex periodic semidiscretisation

---
+++ test/conservation_laws/quartic_nonconvex_test.jl
@@ -0,0 +1,35 @@
+using Test, SummationByPartsOperators
+using OrdinaryDiffEq, DiffEqCallbacks
+
+for T in (Float32, Float64), split_form in (Val{true}(), Val{false}())
+    xmin = T(-1)
+    xmax = T(1)
+    N = 2^6
+    u0func = sinpi
+    tspan = (zero(T), one(T))
+
+    # Fourier
+    let D = fourier_derivative_operator(xmin, xmax, N)
+        Di = dissipation_operator(TadmorWaagan2012Standard(), D)
+        semidisc = CubicPeriodicSemidiscretisation(D, Di, split_form)
+        ode = semidiscretise(u0func, semidisc, tspan)
+        du = similar(ode.u0)
+        semidisc(du, ode.u0, nothing, first(tspan))
+
+        saving = SavingCallback(semidisc, saveat=range(tspan..., length=10))
+        sol = solve(ode, SSPRK104(), dt=1/5N, save_everystep=false, callback=saving)
+    end
+
+    # Periodic FD
+    for acc_order in 2:2:8
+        D = periodic_derivative_operator(1, acc_order, xmin, xmax, N)
+        Di = dissipation_operator(D)
+        semidisc = CubicPeriodicSemidiscretisation(D, Di, split_form)
+        ode = semidiscretise(u0func, semidisc, tspan)
+        du = similar(ode.u0)
+        semidisc(du, ode.u0, nothing, first(tspan))
+
+        saving = SavingCallback(semidisc, saveat=range(tspan..., length=10))
+        sol = solve(ode, SSPRK104(), dt=1/5N, save_everystep=false, callback=saving)
+    end
+end
