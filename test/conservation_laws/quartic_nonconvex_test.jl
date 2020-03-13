223cd4d4fcfdf9276b13582acbbbc4a2437662e0
Hendrik Ranocha <mail@ranocha.de>
2020-03-13 10:27:45 +0300

    add tests for quartic nonconvex flux

--- test/conservation_laws/quartic_nonconvex_test.jl
+++ test/conservation_laws/quartic_nonconvex_test.jl
@@ -6,32 +6,32 @@ for T in (Float32, Float64), split_form in (Val{true}(), Val{false}())
     xmax = T(1)
     N = 2^6
     u0func = sinpi
-    tspan = (zero(T), one(T))
+    tspan = (zero(T), one(T)/50)
 
     # Fourier
     let D = fourier_derivative_operator(xmin, xmax, N)
         Di = dissipation_operator(TadmorWaagan2012Standard(), D)
-        semidisc = CubicPeriodicSemidiscretisation(D, Di, split_form)
-        println(semidisc, devnull)
+        semidisc = QuarticNonconvexPeriodicSemidiscretisation(D, Di, split_form)
+        println(devnull, semidisc)
         ode = semidiscretise(u0func, semidisc, tspan)
         du = similar(ode.u0)
         semidisc(du, ode.u0, nothing, first(tspan))
 
         saving = SavingCallback(semidisc, saveat=range(tspan..., length=10))
-        sol = solve(ode, SSPRK104(), dt=1/5N, save_everystep=false, callback=saving)
+        sol = solve(ode, SSPRK104(), dt=1/20N, save_everystep=false, callback=saving)
     end
 
     # Periodic FD
     for acc_order in 2:2:8
         D = periodic_derivative_operator(1, acc_order, xmin, xmax, N)
         Di = dissipation_operator(D)
-        semidisc = CubicPeriodicSemidiscretisation(D, Di, split_form)
-        println(semidisc, devnull)
+        semidisc = QuarticNonconvexPeriodicSemidiscretisation(D, Di, split_form)
+        println(devnull, semidisc)
         ode = semidiscretise(u0func, semidisc, tspan)
         du = similar(ode.u0)
         semidisc(du, ode.u0, nothing, first(tspan))
 
         saving = SavingCallback(semidisc, saveat=range(tspan..., length=10))
-        sol = solve(ode, SSPRK104(), dt=1/5N, save_everystep=false, callback=saving)
+        sol = solve(ode, SSPRK104(), dt=1/20N, save_everystep=false, callback=saving)
     end
 end
