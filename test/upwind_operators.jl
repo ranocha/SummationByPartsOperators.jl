a5f004199c62d75a318f9bbf7f927819f4aa1e66
Hendrik Ranocha <mail@ranocha.de>
2020-03-25 10:39:40 +0300

    upwind operators of Mattsson(2017), order 3

--- test/upwind_operators.jl
+++ test/upwind_operators.jl
@@ -9,22 +9,34 @@ using SummationByPartsOperators
   xmax = (N + 1) // 1
   interior = 10:N-10
 
-  acc_order = 2
-  Dp_bounded = derivative_operator(Mattsson2017(:plus   ), 1, acc_order, xmin, xmax, N)
-  Dm_bounded = derivative_operator(Mattsson2017(:minus  ), 1, acc_order, xmin, xmax, N)
-  Dc_bounded = derivative_operator(Mattsson2017(:central), 1, acc_order, xmin, xmax, N)
-  println(devnull, Dp_bounded)
-  M = mass_matrix(Dp_bounded)
-  @test M == mass_matrix(Dm_bounded)
-  @test M == mass_matrix(Dc_bounded)
-  Dp_periodic = periodic_derivative_operator(1, acc_order, xmin, xmax, N, -(acc_order - 1) ÷ 2)
-  Dm_periodic = periodic_derivative_operator(1, acc_order, xmin, xmax, N, -acc_order + (acc_order - 1) ÷ 2)
-  Dp = Matrix(Dp_bounded)
-  Dm = Matrix(Dm_bounded)
-  @test Dp[interior,interior] ≈ Matrix(Dp_periodic)[interior,interior]
-  @test Dm[interior,interior] ≈ Matrix(Dm_periodic)[interior,interior]
-  res = M * Dp + Dm' * M
-  res[1,1] += 1
-  res[end,end] -= 1
-  @test norm(res) < 10N * eps()
+
+  for acc_order in 2:3
+    Dp_bounded = derivative_operator(Mattsson2017(:plus   ), 1, acc_order, xmin, xmax, N)
+    Dm_bounded = derivative_operator(Mattsson2017(:minus  ), 1, acc_order, xmin, xmax, N)
+    Dc_bounded = derivative_operator(Mattsson2017(:central), 1, acc_order, xmin, xmax, N)
+    println(devnull, Dp_bounded)
+    M = mass_matrix(Dp_bounded)
+    @test M == mass_matrix(Dm_bounded)
+    @test M == mass_matrix(Dc_bounded)
+    Dp_periodic = periodic_derivative_operator(1, acc_order, xmin, xmax, N, -(acc_order - 1) ÷ 2)
+    Dm_periodic = periodic_derivative_operator(1, acc_order, xmin, xmax, N, -acc_order + (acc_order - 1) ÷ 2)
+    Dp = Matrix(Dp_bounded)
+    Dm = Matrix(Dm_bounded)
+    @test Dp[interior,interior] ≈ Matrix(Dp_periodic)[interior,interior]
+    @test Dm[interior,interior] ≈ Matrix(Dm_periodic)[interior,interior]
+    res = M * Dp + Dm' * M
+    res[1,1] += 1
+    res[end,end] -= 1
+    @test norm(res) < 10N * eps()
+    x = grid(Dp_bounded)
+    for D in (Dp_bounded, Dm_bounded, Dc_bounded)
+      @test norm(D * x.^0) < 10N * eps()
+      for k in 1:acc_order÷2
+        @test D * x.^k ≈ k .* x.^(k-1)
+      end
+      for k in acc_order÷2+1:acc_order
+        @test (D * x.^k)[interior] ≈ (k .* x.^(k-1))[interior]
+      end
+    end
+  end
 end
