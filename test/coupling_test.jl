99b494cb072961fd3e189326be0272dc6895c4d9
Hendrik Ranocha <mail@ranocha.de>
2020-03-30 09:06:55 +0300

    fix conversion of upwind operators to BandedMatrix

--- test/coupling_test.jl
+++ test/coupling_test.jl
@@ -243,3 +243,100 @@ end
     end
   end
 end
+
+
+@testset "Coupled upwind operators" begin
+  for T in (Float32, Float64)
+    for acc_order in 2:7
+      ymin = T(0)
+      ymax = T(2)
+      Dp = derivative_operator(Mattsson2017(:plus ), 1, acc_order, ymin, ymax, 31)
+      Dm = derivative_operator(Mattsson2017(:minus), 1, acc_order, ymin, ymax, 31)
+
+      for N in 1:3
+        xmin = T(-1)
+        xmax = T(2)
+        mesh = UniformMesh1D(xmin, xmax, N)
+        cDp_continuous = couple_continuosly(Dp, mesh)
+        cDm_continuous = couple_continuosly(Dm, mesh)
+        cDp_central    = couple_discontinuosly(Dp, mesh)
+        cDm_central    = couple_discontinuosly(Dm, mesh)
+        cDp_plus       = couple_discontinuosly(Dp, mesh, Val(:plus))
+        cDm_minus      = couple_discontinuosly(Dm, mesh, Val(:minus))
+        for cD in (cDp_continuous, cDm_continuous, cDp_central, cDm_central, cDp_plus, cDm_minus)
+          print(devnull, cD)
+          x = grid(cD)
+          @test norm(cD * x.^0) < 100N * eps(T)
+          @test accuracy_order(cD) == accuracy_order(Dp) == accuracy_order(Dm)
+
+          u = sinpi.(x)
+          v = copy(u)
+          SummationByPartsOperators.scale_by_mass_matrix!(u, cD)
+          SummationByPartsOperators.scale_by_inverse_mass_matrix!(u, cD)
+          @test u ≈ v
+          @test integrate(u, cD) ≈ sum(mass_matrix(cD) * u)
+          @test integrate(u->u^2, u, cD) ≈ sum(u' * mass_matrix(cD) * u)
+          @test cD * u ≈ BandedMatrix(cD) * u
+          @test cD * u ≈ Matrix(cD) * u
+          @test cD * u ≈ sparse(cD) * u
+        end
+        for (cDp,cDm) in ((cDp_continuous,cDm_continuous), (cDp_central,cDm_central), (cDp_plus,cDm_minus))
+          cDp_dense = Matrix(cDp)
+          cDm_dense = Matrix(cDm)
+          M = mass_matrix(cDp)
+          @test M ≈ mass_matrix(cDm)
+          @test sum(M) ≈ xmax - xmin
+
+          diss = M * (cDp_dense - cDm_dense)
+          @test diss ≈ diss'
+          @test maximum(eigvals(Symmetric(diss))) < 100N * eps(T)
+          res = M * cDp_dense + cDm_dense' * M
+          res[1, 1] += 1
+          res[end, end] -= 1
+          @test norm(res) < 100N * eps(T)
+        end
+      end
+
+      for N in 1:3
+        xmin = T(-1)
+        xmax = T(2)
+        mesh = UniformPeriodicMesh1D(xmin, xmax, N)
+        cDp_continuous = couple_continuosly(Dp, mesh)
+        cDm_continuous = couple_continuosly(Dm, mesh)
+        cDp_central    = couple_discontinuosly(Dp, mesh)
+        cDm_central    = couple_discontinuosly(Dm, mesh)
+        cDp_plus       = couple_discontinuosly(Dp, mesh, Val(:plus))
+        cDm_minus      = couple_discontinuosly(Dm, mesh, Val(:minus))
+        for cD in (cDp_continuous, cDm_continuous, cDp_central, cDm_central, cDp_plus, cDm_minus)
+          print(devnull, cD)
+          x = grid(cD)
+          @test norm(cD * x.^0) < 100N * eps(T)
+          @test accuracy_order(cD) == accuracy_order(Dp) == accuracy_order(Dm)
+
+          u = sinpi.(x)
+          v = copy(u)
+          SummationByPartsOperators.scale_by_mass_matrix!(u, cD)
+          SummationByPartsOperators.scale_by_inverse_mass_matrix!(u, cD)
+          @test u ≈ v
+          @test integrate(u, cD) ≈ sum(mass_matrix(cD) * u)
+          @test integrate(u->u^2, u, cD) ≈ sum(u' * mass_matrix(cD) * u)
+          @test cD * u ≈ Matrix(cD) * u
+          @test cD * u ≈ sparse(cD) * u
+        end
+        for (cDp,cDm) in ((cDp_continuous,cDm_continuous), (cDp_central,cDm_central), (cDp_plus,cDm_minus))
+          cDp_dense = Matrix(cDp)
+          cDm_dense = Matrix(cDm)
+          M = mass_matrix(cDp)
+          @test M ≈ mass_matrix(cDm)
+          @test sum(M) ≈ xmax - xmin
+
+          diss = M * (cDp_dense - cDm_dense)
+          @test diss ≈ diss'
+          @test maximum(eigvals(Symmetric(diss))) < 100N * eps(T)
+          res = M * cDp_dense + cDm_dense' * M
+          @test norm(res) < 100N * eps(T)
+        end
+      end
+    end
+  end
+end
