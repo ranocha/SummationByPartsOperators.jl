b2cd032a0fbe9753f4fad251e26227e874652f5a
Hendrik Ranocha <ranocha@users.noreply.github.com>
2022-09-01 08:22:06 +0200

    more fine-grained testsets for Burgers' eq.

--- test/conservation_laws/burgers_test.jl
+++ test/conservation_laws/burgers_test.jl
@@ -7,45 +7,49 @@ for T in (Float32, Float64), split_form in (Val{true}(), Val{false}())
     u0func = sinpi
     tspan = (zero(T), one(T))
 
-    # Fourier
-    let D = fourier_derivative_operator(xmin, xmax, N)
-        Di = dissipation_operator(TadmorWaagan2012Standard(), D)
-        semi = BurgersPeriodicSemidiscretization(D, Di, split_form)
-        println(devnull, semi)
-        ode = semidiscretize(u0func, semi, tspan)
-        du = similar(ode.u0)
-        semi(du, ode.u0, nothing, first(tspan))
+    @testset "Fourier" begin
+        let D = fourier_derivative_operator(xmin, xmax, N)
+            Di = dissipation_operator(TadmorWaagan2012Standard(), D)
+            semi = BurgersPeriodicSemidiscretization(D, Di, split_form)
+            println(devnull, semi)
+            ode = semidiscretize(u0func, semi, tspan)
+            du = similar(ode.u0)
+            semi(du, ode.u0, nothing, first(tspan))
+        end
+       end
+
+    @testset "Periodic FD" begin
+        for acc_order in 2:2:8
+            D = periodic_derivative_operator(1, acc_order, xmin, xmax, N)
+            Di = dissipation_operator(D)
+            semi = BurgersPeriodicSemidiscretization(D, Di, split_form)
+            println(devnull, semi)
+            ode = semidiscretize(u0func, semi, tspan)
+            du = similar(ode.u0)
+            semi(du, ode.u0, nothing, first(tspan))
+        end
     end
 
-    # Periodic FD
-    for acc_order in 2:2:8
-        D = periodic_derivative_operator(1, acc_order, xmin, xmax, N)
-        Di = dissipation_operator(D)
-        semi = BurgersPeriodicSemidiscretization(D, Di, split_form)
-        println(devnull, semi)
-        ode = semidiscretize(u0func, semi, tspan)
-        du = similar(ode.u0)
-        semi(du, ode.u0, nothing, first(tspan))
+    @testset "Legendre" begin
+        let D = legendre_derivative_operator(xmin, xmax, N)
+            Di = nothing
+            semi = BurgersNonperiodicSemidiscretization(D, Di, split_form, zero, zero)
+            println(devnull, semi)
+            ode = semidiscretize(u0func, semi, tspan)
+            du = similar(ode.u0)
+            semi(du, ode.u0, nothing, first(tspan))
+        end
     end
 
-    # Legendre
-    let D = legendre_derivative_operator(xmin, xmax, N)
-        Di = nothing
-        semi = BurgersNonperiodicSemidiscretization(D, Di, split_form, zero, zero)
-        println(devnull, semi)
-        ode = semidiscretize(u0func, semi, tspan)
-        du = similar(ode.u0)
-        semi(du, ode.u0, nothing, first(tspan))
-    end
-
-    # SBP FD
-    for acc_order in 2:2:8
-        D = derivative_operator(MattssonSvärdNordström2004(), 1, acc_order, xmin, xmax, N)
-        Di = dissipation_operator(D)
-        semi = BurgersNonperiodicSemidiscretization(D, Di, split_form, zero, zero)
-        println(devnull, semi)
-        ode = semidiscretize(u0func, semi, tspan)
-        du = similar(ode.u0)
-        semi(du, ode.u0, nothing, first(tspan))
+    @testset "SBP FD"
+        for acc_order in 2:2:8
+            D = derivative_operator(MattssonSvärdNordström2004(), 1, acc_order, xmin, xmax, N)
+            Di = dissipation_operator(D)
+            semi = BurgersNonperiodicSemidiscretization(D, Di, split_form, zero, zero)
+            println(devnull, semi)
+            ode = semidiscretize(u0func, semi, tspan)
+            du = similar(ode.u0)
+            semi(du, ode.u0, nothing, first(tspan))
+        end
     end
 end
