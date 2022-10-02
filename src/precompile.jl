2567afe33d958ff3c4361d40e06ce61919d700f8
Hendrik Ranocha <mail@ranocha.de>
2022-10-02 13:14:42 +0200

    include some explicit precompilation

---
+++ src/precompile.jl
@@ -0,0 +1,16 @@
+
+using SnoopPrecompile: @precompile_all_calls
+
+@precompile_all_calls begin
+  let D = periodic_derivative_operator(derivative_order=1, accuracy_order=2,
+                                        xmin=0.0, xmax=2.0, N=10)
+    u = zeros(eltype(D), size(D, 2))
+    D * u
+  end
+
+  let D = derivative_operator(MattssonNordström2004(), derivative_order=1, accuracy_order=2,
+                              xmin=0.0, xmax=2.0, N=10)
+    u = zeros(eltype(D), size(D, 2))
+    D * u
+  end
+end
