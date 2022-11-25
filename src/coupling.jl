e4eb1b217cddde943b4b7c493d27393459152748
Hendrik Ranocha <mail@ranocha.de>
2022-11-25 16:40:27 +0100

    docstrings of couplind functions

--- src/coupling.jl
+++ src/coupling.jl
@@ -251,6 +251,26 @@ function right_boundary_weight(cD::UniformCoupledOperator)
   factor * right_boundary_weight(D)
 end
 
+
+"""
+    couple_continuously(D, mesh)
+
+Return a derivative operator corresponding to a continuous coupling of `D` on
+the cells of the given `mesh` as in (nodal) continuous Galerkin (CG) methods.
+If the underlying SBP operators are [`LegendreDerivativeOperator`](@ref)s,
+these are CG spectral element methods (CGSEM). However, a continuous coupling
+of arbitrary SBP operators is supported.
+
+The `mesh` can be a [`UniformMesh1D`](@ref) or a [`UniformPeriodicMesh1D`](@ref).
+
+## References
+
+- Ranocha, Mitsotakis, Ketcheson (2021).
+  A Broad Class of Conservative Numerical Methods for Dispersive Wave Equations.
+  [DOI: 10.4208/cicp.OA-2020-0119](https://doi.org/10.4208/cicp.OA-2020-0119)
+"""
+function couple_continuously end
+
 function couple_continuously(D::AbstractNonperiodicDerivativeOperator, mesh::UniformMesh1D)
   UniformNonperiodicCoupledOperator(D, mesh, Val(:continuous))
 end
@@ -258,6 +278,29 @@ end
 function couple_continuously(D::AbstractNonperiodicDerivativeOperator, mesh::UniformPeriodicMesh1D)
   UniformPeriodicCoupledOperator(D, mesh, Val(:continuous))
 end
+
+
+"""
+    couple_discontinuously(D, mesh, [coupling=Val(:central)])
+
+Return a derivative operator corresponding to a discontinuous coupling of `D` on
+the cells of the given `mesh` as in (nodal) discontinuous Galerkin (CG) methods.
+If the underlying SBP operators are [`LegendreDerivativeOperator`](@ref)s,
+these are DG spectral element methods (DGSEM). However, a discontinuous coupling
+of arbitrary SBP operators is supported.
+
+The `mesh` can be a [`UniformMesh1D`](@ref) or a [`UniformPeriodicMesh1D`](@ref).
+The `coupling` can be
+- `Val(:central)` (default), resulting in classical SBP properties
+- `Val(:minus)` or `Val(:plus)`, resulting in upwind SBP operators
+
+## References
+
+- Ranocha, Mitsotakis, Ketcheson (2021).
+  A Broad Class of Conservative Numerical Methods for Dispersive Wave Equations.
+  [DOI: 10.4208/cicp.OA-2020-0119](https://doi.org/10.4208/cicp.OA-2020-0119)
+"""
+function couple_continuously end
 
 function couple_discontinuously(D::AbstractNonperiodicDerivativeOperator, mesh::UniformMesh1D, coupling::Union{Val{:plus}, Val{:central}, Val{:minus}}=Val(:central))
   UniformNonperiodicCoupledOperator(D, mesh, coupling)
