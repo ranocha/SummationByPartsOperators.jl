ab7d28d6f592f0d886714cbc896f9a8afb3ed25a
Hendrik Ranocha <mail@ranocha.de>
2022-11-25 17:29:35 +0100

    add tests

--- test/upwind_operators_test.jl
+++ test/upwind_operators_test.jl
@@ -15,9 +15,9 @@ using SummationByPartsOperators
     Dm_bounded = derivative_operator(Mattsson2017(:minus  ), 1, acc_order, xmin, xmax, N)
     Dc_bounded = derivative_operator(Mattsson2017(:central), 1, acc_order, xmin, xmax, N)
     for compact in (true, false)
-      show(IOContext(devnull, :compact=>false), Dp_bounded)
-      show(IOContext(devnull, :compact=>false), Dm_bounded)
-      show(IOContext(devnull, :compact=>false), Dc_bounded)
+      show(IOContext(devnull, :compact=>compact), Dp_bounded)
+      show(IOContext(devnull, :compact=>compact), Dm_bounded)
+      show(IOContext(devnull, :compact=>compact), Dc_bounded)
     end
     M = mass_matrix(Dp_bounded)
     @test M == mass_matrix(Dm_bounded)
@@ -46,4 +46,45 @@ using SummationByPartsOperators
     @test diss ≈ diss'
     @test maximum(eigvals(Symmetric(diss))) < N * eps()
   end
+end
+
+@testset "UpwindOperators" begin
+  N = 21
+  xmin = 0.
+  xmax = Float64(N + 1)
+  interior = 10:N-10
+  acc_order = 2
+
+  D = upwind_operators(Mattsson2017, derivative_order=1, accuracy_order=acc_order,
+                       xmin=xmin, xmax=xmax, N=N)
+  Dp = derivative_operator(Mattsson2017(:plus   ), 1, acc_order, xmin, xmax, N)
+  Dm = derivative_operator(Mattsson2017(:minus  ), 1, acc_order, xmin, xmax, N)
+  Dc = derivative_operator(Mattsson2017(:central), 1, acc_order, xmin, xmax, N)
+  for compact in (true, false)
+    show(IOContext(devnull, :compact=>compact), D)
+    show(IOContext(devnull, :compact=>compact), Dp)
+    show(IOContext(devnull, :compact=>compact), Dm)
+    show(IOContext(devnull, :compact=>compact), Dc)
+  end
+  @test D.minus == Dm
+  @test D.plus == Dp
+  @test D.central == Dc
+  @test derivative_order(D) == 1
+
+  @test grid(D) == grid(Dp)
+  @test SummationByPartsOperators.xmin(D) == SummationByPartsOperators.xmin(Dp)
+  @test SummationByPartsOperators.xmax(D) == SummationByPartsOperators.xmax(Dp)
+
+  @test mass_matrix(D) == mass_matrix(Dp)
+  @test left_boundary_weight(D) == left_boundary_weight(Dp)
+  @test right_boundary_weight(D) == right_boundary_weight(Dp)
+
+  x = grid(D)
+  @test integrate(x, D) == integrate(x, Dp)
+
+  @test_throws ArgumentError UpwindOperators(
+    derivative_operator(Mattsson2017(:minus  ), 1, acc_order, xmin, xmax, N),
+    derivative_operator(Mattsson2017(:central), 1, acc_order, xmin, xmax, N+1),
+    derivative_operator(Mattsson2017(:plus   ), 1, acc_order, xmin, xmax, N)
+  )
 end
