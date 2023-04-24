ee932f7f34d28b04e9aeb7d886eaea0f2de02e22
Hendrik Ranocha <mail@ranocha.de>
2023-04-24 16:31:35 +0200

    switch to PrecompileTools

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,14 +1,15 @@
-using SnoopPrecompile: @precompile_all_calls
+using PrecompileTools: @compile_workload
 
-@precompile_all_calls begin
-  let D = periodic_derivative_operator(derivative_order=1, accuracy_order=2,
-                                        xmin=0.0, xmax=2.0, N=10)
+@compile_workload begin
+  let D = periodic_derivative_operator(derivative_order = 1, accuracy_order = 2,
+                                       xmin = 0.0, xmax = 2.0, N = 10)
     u = zeros(eltype(D), size(D, 2))
     D * u
   end
 
-  let D = derivative_operator(MattssonNordström2004(), derivative_order=1, accuracy_order=2,
-                              xmin=0.0, xmax=2.0, N=10)
+  let D = derivative_operator(MattssonNordström2004(),
+                              derivative_order = 1, accuracy_order = 2,
+                              xmin = 0.0, xmax = 2.0, N = 10)
     u = zeros(eltype(D), size(D, 2))
     D * u
   end
