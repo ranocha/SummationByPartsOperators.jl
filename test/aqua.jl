4ddff297f37371943a4b2939ec031577fa633a48
Hendrik Ranocha <ranocha@users.noreply.github.com>
2023-04-25 05:47:42 +0200

    switch to PrecompileTools (#181)
    * switch to PrecompileTools
    
    * bump version
    
    * ignore PrecompileTools in Aqua tests

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,6 +2,7 @@ using Aqua
 using SummationByPartsOperators
 
 Aqua.test_all(SummationByPartsOperators;
-  ambiguities=false, # a lot of false positives from dependencies
-  unbound_args=false # TODO: a strange problem I do not understand right now
+  ambiguities = false, # a lot of false positives from dependencies
+  unbound_args = false, # TODO: a strange problem I do not understand right now
+  stale_deps = (; ignore = [:PrecompileTools]),
 )
