108b1d3f171596d022df397b81b07c095196c050
Hendrik Ranocha <ranocha@users.noreply.github.com>
2023-08-11 13:56:08 +0200

    fix issue 208 (#209)
    * fix issue 208
    
    * bump version
    
    * adapt tests and fix typo in second-order fourth-derivative operator

--- test/SBP_operators_test.jl
+++ test/SBP_operators_test.jl
@@ -467,7 +467,7 @@ for source in D_test_list, T in (Float32,Float64)
 end
 
 # Accuracy tests of third derivative operators.
-for source in D_test_list, T in (Float32,Float64)
+@testset "third-derivative operators" for source in D_test_list, T in (Float32,Float64)
     xmin = -one(T)
     xmax = 2*one(T)
     N = 101
@@ -499,16 +499,12 @@ for source in D_test_list, T in (Float32,Float64)
         mul!(res, D, x0)
         @test all(i->abs(res[i]) < eps(T), eachindex(res))
         mul!(res, D, x1)
-        @test all(i->abs(res[i]) < 5000*eps(T), eachindex(res))
-        mul!(res, D, x2)
-        @test all(i->abs(res[i]) < 5000*eps(T), eachindex(res))
+        @test all(i->abs(res[i]) < 50_000*eps(T), eachindex(res))
         # only interior
         mul!(res, D, x2)
-        @test all(i->abs(res[i]) < 5000*eps(T), inner_indices)
+        @test all(i->abs(res[i]) < 160_000*eps(T), inner_indices)
         mul!(res, D, x3)
-        @test all(i->res[i] ≈ 6*x0[i], inner_indices)
-        mul!(res, D, x4)
-        @test any(i->!(res[i] ≈ 24*x1[i]), inner_indices)
+        @test all(i->abs(res[i] - 6*x0[i]) < 340_000*eps(T), inner_indices)
         # boundary: first derivative
         @test abs(derivative_left( D, x0, Val{1}())) < eps(T)
         @test abs(derivative_right(D, x0, Val{1}())) < eps(T)
@@ -519,8 +515,8 @@ for source in D_test_list, T in (Float32,Float64)
         # boundary: second derivative
         @test abs(derivative_left( D, x0, Val{2}())) < eps(T)
         @test abs(derivative_right(D, x0, Val{2}())) < eps(T)
-        @test abs(derivative_left( D, x1, Val{2}())) < eps(T)
-        @test abs(derivative_right(D, x1, Val{2}())) < eps(T)
+        @test abs(derivative_left( D, x1, Val{2}())) < 600 * eps(T)
+        @test abs(derivative_right(D, x1, Val{2}())) < 600 * eps(T)
         @test derivative_left( D, x2, Val{2}()) ≈ 2
         @test derivative_right(D, x2, Val{2}()) ≈ 2
     end
@@ -648,7 +644,7 @@ for source in D_test_list, T in (Float32,Float64)
 end
 
 # Accuracy tests of fourth derivative operators.
-for source in D_test_list, T in (Float32,Float64)
+@testset "fourth-derivative operators" for source in D_test_list, T in (Float32,Float64)
     xmin = -one(T)
     xmax = 2*one(T)
     N = 101
@@ -678,18 +674,16 @@ for source in D_test_list, T in (Float32,Float64)
         @test issymmetric(D) == false
         # interior and boundary
         mul!(res, D, x0)
-        @test all(i->abs(res[i]) < eps(T), eachindex(res))
+        @test all(i->abs(res[i]) < 10 * eps(T) / D.Δx^4, eachindex(res))
         mul!(res, D, x1)
-        @test all(i->abs(res[i]) < 5000*eps(T), eachindex(res))
+        @test all(i->abs(res[i]) < 100 * eps(T) / D.Δx^4, eachindex(res))
         mul!(res, D, x2)
-        @test all(i->abs(res[i]) < 5000*eps(T), eachindex(res))
+        @test all(i->abs(res[i]) < 100 * eps(T) / D.Δx^4, eachindex(res))
         # only interior
         mul!(res, D, x2)
-        @test all(i->abs(res[i]) < 5000*eps(T), inner_indices)
+        @test all(i->abs(res[i]) < 100 * eps(T) / D.Δx^4, inner_indices)
         mul!(res, D, x3)
-        @test all(i->abs(res[i]) < 5000*eps(T), inner_indices)
-        mul!(res, D, x4)
-        @test any(i->!(res[i] ≈ 24*x0[i]), inner_indices)
+        @test all(i->abs(res[i]) < 100 * eps(T) / D.Δx^4, inner_indices)
         # boundary: first derivative
         @test abs(derivative_left( D, x0, Val{1}())) < eps(T)
         @test abs(derivative_right(D, x0, Val{1}())) < eps(T)
@@ -700,17 +694,17 @@ for source in D_test_list, T in (Float32,Float64)
         # boundary: second derivative
         @test abs(derivative_left( D, x0, Val{2}())) < eps(T)
         @test abs(derivative_right(D, x0, Val{2}())) < eps(T)
-        @test abs(derivative_left( D, x1, Val{2}())) < eps(T)
-        @test abs(derivative_right(D, x1, Val{2}())) < eps(T)
-        @test derivative_left( D, x2, Val{2}()) ≈ one(T)
-        @test derivative_right(D, x2, Val{2}()) ≈ one(T)
+        @test abs(derivative_left( D, x1, Val{2}())) < 600 * eps(T)
+        @test abs(derivative_right(D, x1, Val{2}())) < 600 * eps(T)
+        @test derivative_left( D, x2, Val{2}()) ≈ 2 * one(T)
+        @test derivative_right(D, x2, Val{2}()) ≈ 2 * one(T)
         # boundary: third derivative
-        @test abs(derivative_left( D, x0, Val{3}())) < eps(T)
-        @test abs(derivative_right(D, x0, Val{3}())) < eps(T)
-        @test abs(derivative_left( D, x1, Val{3}())) < eps(T)
-        @test abs(derivative_right(D, x1, Val{3}())) < eps(T)
-        @test abs(derivative_left( D, x2, Val{3}())) < eps(T)
-        @test abs(derivative_right(D, x2, Val{3}())) < eps(T)
+        @test abs(derivative_left( D, x0, Val{3}())) < 10 * eps(T) / D.Δx^3
+        @test abs(derivative_right(D, x0, Val{3}())) < 10 * eps(T) / D.Δx^3
+        @test abs(derivative_left( D, x1, Val{3}())) < 10 * eps(T) / D.Δx^3
+        @test abs(derivative_right(D, x1, Val{3}())) < 10 * eps(T) / D.Δx^3
+        @test abs(derivative_left( D, x2, Val{3}())) < 10 * eps(T) / D.Δx^3
+        @test abs(derivative_right(D, x2, Val{3}())) < 10 * eps(T) / D.Δx^3
     end
 
     acc_order = 4
@@ -881,4 +875,16 @@ for T in (Float32, Float64), acc_order in (2,4,6,8)
     @test all(i->dest1[i] ≈ dest3[i], eachindex(u))
     dest3 = D_safe*u
     @test all(i->dest1[i] ≈ dest3[i], eachindex(u))
+end
+
+
+# https://github.com/ranocha/SummationByPartsOperators.jl/issues/208
+@testset "issue #208" begin
+    for acc_order in (2, 4, 6), der_order in 1:4
+        @test_nowarn derivative_operator(Mattsson2014(),
+                                         derivative_order = der_order,
+                                         accuracy_order = acc_order,
+                                         xmin = -1.0, xmax = 1.0,
+                                         N = 20)
+    end
 end
