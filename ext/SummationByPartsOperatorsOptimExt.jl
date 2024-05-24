7fe321759802bf60b0a840fa3abedeae13f7a5ab
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-05-24 19:07:27 +0200

    some spaces

--- ext/SummationByPartsOperatorsOptimExt.jl
+++ ext/SummationByPartsOperatorsOptimExt.jl
@@ -9,6 +9,7 @@ end
 using SummationByPartsOperators: SummationByPartsOperators, GlaubitzNordströmÖffner2023
 
 function SummationByPartsOperators.construct_function_space_operator(basis, x_min, x_max, nodes, ::GlaubitzNordströmÖffner2023)
+    # TODO: implement the construction of the function space operator by solving an optimization problem
     N = length(nodes)
     weights = ones(N)
     D = zeros(N, N)
