7115ae91d2769b62f4c883468d2f50f717173d84
Hendrik Ranocha <ranocha@users.noreply.github.com>
2025-11-13 16:48:46 +0100

    Fix doc preview for dependabot PRs (#372)
    * Fix doc preview for dependabot PRs
    
    Add condition to push previews based on environment variables. See https://github.com/JuliaDocs/Documenter.jl/issues/2048#issuecomment-3509075327
    
    * format
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/make.jl
+++ docs/make.jl
@@ -88,4 +88,7 @@ makedocs(modules = [SummationByPartsOperators],
 
 deploydocs(repo = "github.com/ranocha/SummationByPartsOperators.jl",
            devbranch = "main",
-           push_preview = true)
+           # Only push previews if all the relevant environment variables are non-empty.
+           push_preview = all(!isempty,
+                              (get(ENV, "GITHUB_TOKEN", ""),
+                               get(ENV, "DOCUMENTER_KEY", ""))))
