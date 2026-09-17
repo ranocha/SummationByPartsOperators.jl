64da41a2c8cf5f7ebdb32406f4aa26e46d1334ab
Hendrik Ranocha <mail@ranocha.de>
2026-09-17 08:45:57 +0200

    introduce mul_internal! to fix ambiguities on Julia 1.13

--- src/periodic_operators.jl
+++ src/periodic_operators.jl
@@ -49,9 +49,10 @@ The coefficients of a derivative operator on a periodic grid.
 end
 
 # Compute `α*D*u + β*dest` and store the result in `dest`.
-function mul!(dest::AbstractVector,
-              coefficients::PeriodicDerivativeCoefficients{T, LowerOffset, UpperOffset},
-              u::AbstractVector, α, β) where {T, LowerOffset, UpperOffset}
+function mul_internal!(dest::AbstractVector,
+                       coefficients::PeriodicDerivativeCoefficients{T, LowerOffset,
+                                                                    UpperOffset},
+                       u::AbstractVector, α, β) where {T, LowerOffset, UpperOffset}
     @boundscheck begin
         @argcheck length(dest)==length(u) DimensionMismatch
         @argcheck length(u)>LowerOffset + UpperOffset DimensionMismatch
@@ -66,9 +67,10 @@ function mul!(dest::AbstractVector,
 end
 
 # Compute `α*D*u` and store the result in `dest`.
-function mul!(dest::AbstractVector,
-              coefficients::PeriodicDerivativeCoefficients{T, LowerOffset, UpperOffset},
-              u::AbstractVector, α) where {T, LowerOffset, UpperOffset}
+function mul_internal!(dest::AbstractVector,
+                       coefficients::PeriodicDerivativeCoefficients{T, LowerOffset,
+                                                                    UpperOffset},
+                       u::AbstractVector, α) where {T, LowerOffset, UpperOffset}
     @boundscheck begin
         @argcheck length(dest)==length(u) DimensionMismatch
         @argcheck length(u)>LowerOffset + UpperOffset DimensionMismatch
@@ -748,7 +750,7 @@ Base.@propagate_inbounds function mul!(dest::AbstractVector, D::PeriodicDerivati
         @argcheck size(D, 2)==length(u) DimensionMismatch
         @argcheck size(D, 1)==length(dest) DimensionMismatch
     end
-    @inbounds mul!(dest, D.coefficients, u, α * D.factor, β)
+    @inbounds mul_internal!(dest, D.coefficients, u, α * D.factor, β)
 end
 
 # Compute `α*D*u` and store the result in `dest`.
@@ -758,7 +760,7 @@ Base.@propagate_inbounds function mul!(dest::AbstractVector, D::PeriodicDerivati
         @argcheck size(D, 2)==length(u) DimensionMismatch
         @argcheck size(D, 1)==length(dest) DimensionMismatch
     end
-    @inbounds mul!(dest, D.coefficients, u, α * D.factor)
+    @inbounds mul_internal!(dest, D.coefficients, u, α * D.factor)
 end
 
 """
